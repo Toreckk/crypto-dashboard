a0f8a1ad10bbfbc2bbd11e1dd17e1ee3474bbbee
Juan Coret <coret.juan@gmail.com>
2021-03-09 20:26:45 +0100

    Added cryptocompare

--- src/App/AppProvider.tsx
+++ src/App/AppProvider.tsx
@@ -1,21 +1,10 @@
-import React, { createContext, useState, FC } from "react";
+import React, { createContext, useState, FC, useEffect } from "react";
+const cc = require('cryptocompare');
 
-type ContextStateTypes = {
-    page: String,
-    firstVisit: Boolean,
-    setFirstVisit: (firstVisit: Boolean)=>void,
-    setPage: (page: String)=> void
-}
 
-const confirmFavorites: any  = () => {
-    return {
-        firstVisit: false,
-        page: 'dashboard',
-    }
-}
 
 const savedSettings: any = () =>{
-    let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash') || '{}');
+    // let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash') || '{}');
     console.log(localStorage.getItem('cryptoDash') === null);
     if(localStorage.getItem('cryptoDash') === null){
         return {
@@ -26,9 +15,18 @@ const savedSettings: any = () =>{
     return {};
 }
 
+type ContextStateTypes = {
+    page: String,
+    firstVisit: Boolean,
+    coinList: [],
+    setFirstVisit: (firstVisit: Boolean)=>void,
+    setPage: (page: String)=> void
+}
+
 const AppContextDefaultValues: ContextStateTypes = {
     page: "dashboard",
     firstVisit: false,
+    coinList: [],
     ...savedSettings()
 
 };
@@ -38,11 +36,17 @@ export const AppContext = createContext<ContextStateTypes>(AppContextDefaultValu
 const AppProvider: FC = ({children}) => {
     const [page, setPage] = useState(AppContextDefaultValues.page);
     const [firstVisit, setFirstVisit] = useState(AppContextDefaultValues.firstVisit);
-    console.log(`page: ${page} firstVisit:${firstVisit}`);
-    
+    const [coinList, setCoinList] = useState(AppContextDefaultValues.coinList);
+
+    useEffect(() => {
+        const fetchCoins = async () => {
+            setCoinList( (await cc.coinList()).Data);
+        }
+        fetchCoins();
+    });
 
     return (
-        <AppContext.Provider value={{page, setPage, firstVisit, setFirstVisit}}>
+        <AppContext.Provider value={{page, setPage, firstVisit, setFirstVisit, coinList}}>
             {children}
         </AppContext.Provider>
     );
