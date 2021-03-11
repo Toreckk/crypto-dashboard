fdbc5a5d8a7d9968c55370ea22afc495ef14e8ff
Juan Coret <coret.juan@gmail.com>
2021-03-11 15:50:32 +0100

    Created search component

---
+++ src/Settings/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Search.scss";
+
+export const Search = () =>{
+    return (
+        <div className="search-grid">
+            <h2>Search all coins</h2>
+            <input className="search-input"/>
+        </div>
+    );
+}
